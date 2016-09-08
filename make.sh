23240a230a88ee0a33d420d906e32197d2c492bf
Riccardo Balbo <riccardo@forkforge.net>
2016-09-08 12:20:48 +0200

    .

--- make.sh
+++ make.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Configuration##
-REPO_NAME="riccardobl"
-BRANCH="travis-auto"
-GROUP="org.jmonkeyengine.bullet_builder"
-
-#############
-if [ "$BRANCH" == "travis-auto" ];
+# Configurations
+if [ "$REPO_NAME" == "" ];
 then
-    BRANCH=$TRAVIS_BRANCH
+    REPO_NAME="riccardobl"
 fi
+if [ "$GROUP" == "" ];
+then
+    GROUP="org.jmonkeyengine.bullet_builder"
+fi
+if [ "$BRANCH" == "" ];
+then
+    BRANCH="master"
+fi
+if [ "$VERSION" == "" ];
+then
+    VERSION="1.0"
+fi
+
+###########################
+###########################
+# Init
 REPO="https://github.com/$REPO_NAME/jmonkeyengine.git"
-VERSION="1.1"
 DEPLOY="false" 
 JDK_ROOT="$JAVA_HOME"
 READ_LINK="readlink"
@@ -20,9 +30,7 @@ then
     echo "Use greadlink"
     READ_LINK="greadlink"  
 fi
-
 JDK_ROOT="$($READ_LINK -f `which java` | sed "s:/Commands/java::")"
-
 if [ ! -f "$JDK_ROOT/Headers/jni.h" ];
 then
     JDK_ROOT="$JAVA_HOME"
@@ -39,7 +47,6 @@ then
         fi
     fi
 fi
-
 mkdir -p build
 if [ ! -f "build/bash_colors.sh" ];
 then
@@ -48,23 +55,16 @@ fi
 source build/bash_colors.sh
 
 
-function cleanTMP {
-    rm -Rf build/tmp
-    mkdir -p build/tmp
-}
 
-function clean {
-    rm -R build
-    mkdir build   
-}
-
+###########################
+###########################
+#Download
 function downloadIfRequired {
     if [ ! -d "build/bullet" ]; 
     then
         download
     fi
 }
-
 function download {
     clr_green "Download Bullet"
     rm -f build/tmp/vhacd.zip
@@ -74,24 +74,27 @@ function download {
     mv build/tmp/ext/* build/bullet
 }
 
+
+
+
+###########################
+###########################
+#Utils
+function cleanTMP {
+    rm -Rf build/tmp
+    mkdir -p build/tmp
+}
+function clean {
+    rm -R build
+    mkdir build   
+}
 function setPlatformArch {
     PLATFORM=$1
     ARCH=$2
     OUT_PATH="$PWD/build/lib/native/$PLATFORM/$ARCH"
     mkdir -p $OUT_PATH
 }
-
-function buildLinux64 {
-    buildLinux "x86_64"
-}
-
-
-function buildLinux32 {
-    buildLinux "x86"
-}
-
-function findCppFiles {
-    #Find c++ files
+function findCppFiles { #Find .cpp and header paths that need to be passed to g++
     echo "">build/tmp/cpplist.txt
     find  build/bullet/src/BulletCollision -type f -name '*.cpp' >> build/tmp/cpplist.txt
     find  build/bullet/src/BulletDynamics -type f -name '*.cpp' >> build/tmp/cpplist.txt
@@ -116,6 +119,11 @@ function findCppFiles {
     done  
 }
 
+
+
+###########################
+###########################
+#Build scripts
 function buildLinux {
     downloadIfRequired
     setPlatformArch "linux" $1
@@ -125,7 +133,6 @@ function buildLinux {
     then
         arch_flag="-m32"
     fi
-    
     findCppFiles
     build_script="
     g++ -mtune=generic -DBT_NO_PROFILE=1 -fpermissive -U_FORTIFY_SOURCE -fPIC -Ofast  $arch_flag -shared
@@ -140,8 +147,6 @@ function buildLinux {
     $build_script
     if [ $? -ne 0 ]; then exit 1; fi
 }
-
-
 function buildWindows {
     downloadIfRequired
     setPlatformArch "windows" $1
@@ -151,10 +156,7 @@ function buildWindows {
     then
         compiler="i686-w64-mingw32-g++"
     fi
-
     findCppFiles
-
-
     build_script="
     $compiler -mtune=generic -DBT_NO_PROFILE=1 -fpermissive -fPIC   -U_FORTIFY_SOURCE -Ofast  -DWIN32  -shared -static
        -Ibuild/bullet/src/
@@ -167,39 +169,17 @@ function buildWindows {
     clr_escape "$(echo $build_script)" $CLR_BOLD $CLR_BLUE
     $build_script
     if [ $? -ne 0 ]; then exit 1; fi
-
 }
-
-function buildWindows32 {
-    buildWindows "x86"   
-}
-
-function buildWindows64 {
-    buildWindows "x86-64"   
-}
-
-function buildMac32 {
-    buildMac "x86"   
-}
-
-function buildMac64 {
-    buildMac "x86_64"   
-}
-
-
 function buildMac {
     downloadIfRequired
     setPlatformArch "osx" $1
     clr_green "Compile for $PLATFORM $ARCH..."
-
     arch_flag="-arch x86_64"
     if [ "$1" = "x86" ];
     then
         arch_flag="-arch i386"
     fi
-
     findCppFiles
-
     build_script="
     g++ -mtune=generic -DBT_NO_PROFILE=1 -fpermissive $arch_flag -U_FORTIFY_SOURCE -fPIC -Ofast   -shared
         -Ibuild/bullet/src/
@@ -214,15 +194,52 @@ function buildMac {
     if [ $? -ne 0 ]; then exit 1; fi
 }
 
+
+
+###########################
+###########################
+#Build Aliases
+function buildWindows32 {
+    buildWindows "x86"   
+}
+function buildWindows64 {
+    buildWindows "x86-64"   
+}
+function buildMac32 {
+    buildMac "x86"   
+}
+function buildMac64 {
+    buildMac "x86_64"   
+}
+function buildLinux64 {
+    buildLinux "x86_64"
+}
+function buildLinux32 {
+    buildLinux "x86"
+}
+function buildAll {
+    buildLinux32 
+    buildLinux64  
+    buildWindows32
+    buildWindows64
+}
+
+
+
+###########################
+###########################
+#Travis
 function travis {
     DEPLOY="false"
+
     VERSION=$TRAVIS_COMMIT
     if [ "$TRAVIS_TAG" != "" ];
     then
         echo "Deploy for $TRAVIS_TAG."
-        VERSION=$TRAVIS_TAG
-        DEPLOY="true"    
+        VERSION=$TRAVIS_TAG #Set version to tag name
+        DEPLOY="true"  #Deploy only on tags
     fi
+    
     BINTRAY_VERSION="$REPO_NAME-$BRANCH-$VERSION"
     BINTRAY_SNAPSHOT="$REPO_NAME-$BRANCH-SNAPSHOT"
 
@@ -237,6 +254,7 @@ function travis {
         rm -Rf deploy
         mkdir -p deploy/
         
+        # Check if windows and linux builds exist
         out=`curl -u$BINTRAY_USER:$BINTRAY_API_KEY --silent --head --write-out '%{http_code}'  -o deploy/tmpl.tar.gz.h  https://dl.bintray.com/riccardo/jme3-bullet-native-files/$BINTRAY_VERSION/libs-winLinux-$BINTRAY_VERSION.tar.gz`
         if [ "$out" != "200" ];
         then
@@ -244,6 +262,7 @@ function travis {
             exit 0
         fi
         
+        # Check if mac build exists
         out=`curl -u$BINTRAY_USER:$BINTRAY_API_KEY --silent --head --write-out '%{http_code}'  -o deploy/tmpm.tar.gz.h https://dl.bintray.com/riccardo/jme3-bullet-native-files/$BINTRAY_VERSION/libs-mac-$BINTRAY_VERSION.tar.gz`
         if [ "$out" != "200" ];
         then
@@ -251,30 +270,29 @@ function travis {
             exit 0
         fi
         
+        # Download linux & windows builds
         curl -u$BINTRAY_USER:$BINTRAY_API_KEY --silent  -o deploy/tmpl.tar.gz https://dl.bintray.com/riccardo/jme3-bullet-native-files/$BINTRAY_VERSION/libs-winLinux-$BINTRAY_VERSION.tar.gz   
        
+       #Download mac build
         curl -u$BINTRAY_USER:$BINTRAY_API_KEY --silent  -o deploy/tmpm.tar.gz https://dl.bintray.com/riccardo/jme3-bullet-native-files/$BINTRAY_VERSION/libs-mac-$BINTRAY_VERSION.tar.gz
       
+
+        # Build artifact
         echo "Deploy!"
-    
         rm -Rf buid/tests/
         mkdir -p build/tests
         mkdir -p build/lib/        
-
-        tar -xzf deploy/tmpl.tar.gz -C build/lib/
+        # Merge both builds together
+        tar -xzf deploy/tmpl.tar.gz -C build/lib/ 
         tar -xzf deploy/tmpm.tar.gz -C build/lib/
-      #  ls -l build/lib/native
+        #Build jar 
         `which jar` cf deploy/jme3-bullet-native-$BINTRAY_VERSION.jar -C build/lib .
+        #Upload on bintray
          target_dir=${GROUP//./\/}
-        curl -X PUT  -T  deploy/jme3-bullet-native-$BINTRAY_VERSION.jar -u$BINTRAY_USER:$BINTRAY_API_KEY \
+         curl -X PUT  -T  deploy/jme3-bullet-native-$BINTRAY_VERSION.jar -u$BINTRAY_USER:$BINTRAY_API_KEY \
 "https://api.bintray.com/content/riccardo/jme3-bullet-native/jme3-bullet-native/$BINTRAY_VERSION/$target_dir/jme3-bullet-native/$BINTRAY_VERSION/jme3-bullet-native-$BINTRAY_VERSION.jar?publish=1&override=1"
-        
         curl -X PUT  -T  deploy/jme3-bullet-native-$BINTRAY_VERSION.jar -u$BINTRAY_USER:$BINTRAY_API_KEY \
 "https://api.bintray.com/content/riccardo/jme3-bullet-native/jme3-bullet-native/$BINTRAY_SNAPSHOT/$target_dir/jme3-bullet-native/$BINTRAY_SNAPSHOT/jme3-bullet-native-$BINTRAY_SNAPSHOT.jar?publish=1&override=1"
-        
-     #   curl -X PUT  -T  build/release/vhacd-native-$VERSION.jar -u$BINTRAY_USER:$BINTRAY_API_KEY\
-       # "https://api.bintray.com/content/riccardo/v-hacd/v-hacd-java-bindings/$VERSION/vhacd/vhacd-native/$VERSION/"
-        
                 
     else
         if [ "$TRAVIS_OS_NAME" = "linux" ];
@@ -285,6 +303,7 @@ function travis {
             buildWindows64
             if [ "$DEPLOY" = "true" ];
             then           
+                #Upload build to a temporary repository
                 mkdir -p deploy/
                 tar -C build/lib/ -czf deploy/libs-winLinux-$BINTRAY_VERSION.tar.gz .
                 curl -X PUT  -T  deploy/libs-winLinux-$BINTRAY_VERSION.tar.gz -u$BINTRAY_USER:$BINTRAY_API_KEY\
@@ -297,6 +316,7 @@ function travis {
             buildMac64
             if [ "$DEPLOY" = "true" ];
             then    
+                #Upload build to a temporary repository
                 mkdir -p deploy/
                 tar -C build/lib/ -czf deploy/libs-mac-$BINTRAY_VERSION.tar.gz .
                 curl -X PUT  -T  deploy/libs-mac-$BINTRAY_VERSION.tar.gz -u$BINTRAY_USER:$BINTRAY_API_KEY\
@@ -309,29 +329,33 @@ function travis {
 
 
 
-function buildAll {
-    buildLinux32 
-    buildLinux64  
-    buildWindows32
-    buildWindows64
+###########################
+###########################
+#Main
+function main(){
+ 
+    if [ "$1" = "" ];
+    then
+        echo "Usage: make.sh target"
+        echo " - Targets: buildAll,buildWindows32,buildWindows64,buildLinux32,buildLinux64,buildMac32,buildMac64,clean"
+        exit 0
+    fi
+
+    cleanTMP
+    if [ ! -d "build/jmonkeyengine" ]; then
+        clr_green "Clone engine $REPO:$BRANCH..."
+        git clone $REPO build/jmonkeyengine
+        cd build/jmonkeyengine
+        git checkout $BRANCH
+        cd ../../
+    fi
+    cp -Rf build/jmonkeyengine build/tmp/jmonkeyengine 
+
+    clr_magenta "Run $1..."
+    $1 ${*:2}
+    clr_magenta "Build complete, results are stored in $PWD/build/"
 }
 
-cleanTMP
-if [ ! -d "build/jmonkeyengine" ]; then
-    clr_green "Clone engine $REPO:$BRANCH..."
-    git clone $REPO build/jmonkeyengine
-    cd build/jmonkeyengine
-    git checkout $BRANCH
-    cd ../../
-fi
-cp -Rf build/jmonkeyengine build/tmp/jmonkeyengine 
 
-if [ "$1" = "" ];
-then
-    echo "Usage: make.sh target"
-    echo " - Targets: buildAll,buildWindows32,buildWindows64,buildLinux32,buildLinux64,buildMac32,buildMac64,clean"
-    exit 0
-fi
-clr_magenta "Run $1..."
-$1 ${*:2}
-clr_magenta "Build complete, results are stored in $PWD/build/"
+#Start script
+main $@
